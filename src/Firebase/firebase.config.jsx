23e7175595dbb18d7f84f63b7138b9bc70f24017
Fahmidur-arnob <arnobdev93@gmail.com>
2023-04-11 14:55:49 +0600

    Made some changes

--- src/Firebase/firebase.config.jsx
+++ src/Firebase/firebase.config.jsx
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCb0_Z5-w1S64HjmLPNVY8buzBa8Emhbvc",
-  authDomain: "doctors-orbit.firebaseapp.com",
-  projectId: "doctors-orbit",
-  storageBucket: "doctors-orbit.appspot.com",
-  messagingSenderId: "310823456315",
-  appId: "1:310823456315:web:e141ba96c7a998c235d61e"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId
 };
 
 // Initialize Firebase
